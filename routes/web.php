e34c9dd29741ded5a9927637c99f0242b42546db
Ismael Filho <ismaelscf@gmail.com>
2023-09-27 09:36:46 -0300

    forçando autenticação de rotas.

--- routes/web.php
+++ routes/web.php
@@ -24,40 +24,40 @@ Route::get('/autocomplete', [App\Http\Controllers\SearchController::class, 'auto
 Route::get('/', [App\Http\Controllers\HomeController::class, 'homeCompact'])->name('home');
 // Route::get('/register', [App\Http\Controllers\HomeController::class, 'homeCompact'])->name('home');
 
-Route::get('/homeComplete', [App\Http\Controllers\HomeController::class, 'index'])->name('homeComplete');
-Route::get('/consumers', [App\Http\Controllers\ConsumerController::class, 'index'])->name('consumers');
-Route::post('/consumers', [App\Http\Controllers\ConsumerController::class, 'store']);
-Route::get('/consumer/{id}', [App\Http\Controllers\ConsumerController::class, 'edit'])->name('consumerEdit');
-Route::post('/consumer/edit/{id}', [App\Http\Controllers\ConsumerController::class, 'update'])->name('consumerEditPost');
-Route::get('/spend', [App\Http\Controllers\ConsumerController::class, 'spend'])->name('spend');
-Route::post('/spend', [App\Http\Controllers\ConsumerController::class, 'spendAdd'])->name('spendAdd');
-Route::get('/spend/accepted/{id}', [App\Http\Controllers\ConsumerController::class, 'accepted'])->name('accepted');
-Route::get('/spend/denied/{id}', [App\Http\Controllers\ConsumerController::class, 'denied'])->name('denied');
-Route::get('/loan', [App\Http\Controllers\LoanController::class, 'index'])->name('loan');
-Route::post('/loan', [App\Http\Controllers\LoanController::class, 'store'])->name('borrow');
-Route::get('/loan/{id}', [App\Http\Controllers\LoanController::class, 'show'])->name('showLoan');
-Route::get('/loan/edit/{id}', [App\Http\Controllers\LoanController::class, 'edit'])->name('editInstallments');  
-Route::get('/loan/cancel/{id}', [App\Http\Controllers\LoanController::class, 'cancel'])->name('cancel');
-Route::post('/loan/renegotiate/{id}', [App\Http\Controllers\LoanController::class, 'renegotiate'])->name('renegotiate');
-Route::get('/loan/finish/{id}', [App\Http\Controllers\LoanController::class, 'finish'])->name('finish');
-Route::get('/collaborators', [App\Http\Controllers\CollaboratorController::class, 'index'])->name('collaborators');
-Route::post('/collaborators', [App\Http\Controllers\CollaboratorController::class, 'store']);
-Route::get('/balance', [App\Http\Controllers\CollaboratorController::class, 'balance'])->name('balance');
-Route::get('/balance/zerar/{id}', [App\Http\Controllers\CollaboratorController::class, 'zerarBalance'])->name('zerarBalance');
-Route::post('/AddBalance', [App\Http\Controllers\CollaboratorController::class, 'AddBalance'])->name('AddBalance');
-Route::get('/collaborator/{id}', [App\Http\Controllers\CollaboratorController::class, 'edit'])->name('collaboratorEdit');
-Route::post('/collaborator/edit/{id}', [App\Http\Controllers\CollaboratorController::class, 'update'])->name('collaboratorEditPost');
-Route::post('/region', [App\Http\Controllers\RegionsController::class, 'store']);
-Route::get('/region/{id}', [App\Http\Controllers\RegionsController::class, 'edit'])->name('regionEdit');
-Route::post('/region/edit/{id}', [App\Http\Controllers\RegionsController::class, 'update'])->name('regionEditPost');
+Route::get('/homeComplete', [App\Http\Controllers\HomeController::class, 'index'])->name('homeComplete')->middleware('auth');
+Route::get('/consumers', [App\Http\Controllers\ConsumerController::class, 'index'])->name('consumers')->middleware('auth');
+Route::post('/consumers', [App\Http\Controllers\ConsumerController::class, 'store'])->middleware('auth');
+Route::get('/consumer/{id}', [App\Http\Controllers\ConsumerController::class, 'edit'])->name('consumerEdit')->middleware('auth');
+Route::post('/consumer/edit/{id}', [App\Http\Controllers\ConsumerController::class, 'update'])->name('consumerEditPost')->middleware('auth');
+Route::get('/spend', [App\Http\Controllers\ConsumerController::class, 'spend'])->name('spend')->middleware('auth');
+Route::post('/spend', [App\Http\Controllers\ConsumerController::class, 'spendAdd'])->name('spendAdd')->middleware('auth');
+Route::get('/spend/accepted/{id}', [App\Http\Controllers\ConsumerController::class, 'accepted'])->name('accepted')->middleware('auth');
+Route::get('/spend/denied/{id}', [App\Http\Controllers\ConsumerController::class, 'denied'])->name('denied')->middleware('auth');
+Route::get('/loan', [App\Http\Controllers\LoanController::class, 'index'])->name('loan')->middleware('auth');
+Route::post('/loan', [App\Http\Controllers\LoanController::class, 'store'])->name('borrow')->middleware('auth');
+Route::get('/loan/{id}', [App\Http\Controllers\LoanController::class, 'show'])->name('showLoan')->middleware('auth');
+Route::get('/loan/edit/{id}', [App\Http\Controllers\LoanController::class, 'edit'])->name('editInstallments')->middleware('auth');  
+Route::get('/loan/cancel/{id}', [App\Http\Controllers\LoanController::class, 'cancel'])->name('cancel')->middleware('auth');
+Route::post('/loan/renegotiate/{id}', [App\Http\Controllers\LoanController::class, 'renegotiate'])->name('renegotiate')->middleware('auth');
+Route::get('/loan/finish/{id}', [App\Http\Controllers\LoanController::class, 'finish'])->name('finish')->middleware('auth');
+Route::get('/collaborators', [App\Http\Controllers\CollaboratorController::class, 'index'])->name('collaborators')->middleware('auth');
+Route::post('/collaborators', [App\Http\Controllers\CollaboratorController::class, 'store'])->middleware('auth');
+Route::get('/balance', [App\Http\Controllers\CollaboratorController::class, 'balance'])->name('balance')->middleware('auth');
+Route::get('/balance/zerar/{id}', [App\Http\Controllers\CollaboratorController::class, 'zerarBalance'])->name('zerarBalance')->middleware('auth');
+Route::post('/AddBalance', [App\Http\Controllers\CollaboratorController::class, 'AddBalance'])->name('AddBalance')->middleware('auth');
+Route::get('/collaborator/{id}', [App\Http\Controllers\CollaboratorController::class, 'edit'])->name('collaboratorEdit')->middleware('auth');
+Route::post('/collaborator/edit/{id}', [App\Http\Controllers\CollaboratorController::class, 'update'])->name('collaboratorEditPost')->middleware('auth');
+Route::post('/region', [App\Http\Controllers\RegionsController::class, 'store'])->middleware('auth');
+Route::get('/region/{id}', [App\Http\Controllers\RegionsController::class, 'edit'])->name('regionEdit')->middleware('auth');
+Route::post('/region/edit/{id}', [App\Http\Controllers\RegionsController::class, 'update'])->name('regionEditPost')->middleware('auth');
 
-Route::get('/inspection', [App\Http\Controllers\InspectionController::class, 'index'])->name('inspection');
-Route::get('/inspection/week', [App\Http\Controllers\InspectionController::class, 'week'])->name('week');
-Route::get('/inspection/month', [App\Http\Controllers\InspectionController::class, 'month'])->name('month');
-Route::get('/inspection/year', [App\Http\Controllers\InspectionController::class, 'year'])->name('year');
+Route::get('/inspection', [App\Http\Controllers\InspectionController::class, 'index'])->name('inspection')->middleware('auth');
+Route::get('/inspection/week', [App\Http\Controllers\InspectionController::class, 'week'])->name('week')->middleware('auth');
+Route::get('/inspection/month', [App\Http\Controllers\InspectionController::class, 'month'])->name('month')->middleware('auth');
+Route::get('/inspection/year', [App\Http\Controllers\InspectionController::class, 'year'])->name('year')->middleware('auth');
 
 
-Route::get('/contact/{id}/{consumerId}', [App\Http\Controllers\ContactsController::class, 'delete'])->name('destroy');
+Route::get('/contact/{id}/{consumerId}', [App\Http\Controllers\ContactsController::class, 'delete'])->name('destroy')->middleware('auth');
 
 
 
