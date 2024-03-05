273f308dbbd12bbb3968c067a14418596ae457d6
Ismael Filho <ismaelscf@gmail.com>
2024-03-05 11:13:57 -0300

    Redirecionamento login colaborador

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,7 +43,7 @@ class HomeController extends Controller
                       ->whereYear("updated_at", $year);
             })
             ->get();            
-            
+
             $loansFinished = Loan::whereDate("updated_at", $day)->where("status", "paid")->get();
 
             // dd($loansFinished);
@@ -52,7 +52,9 @@ class HomeController extends Controller
         elseif( Auth::user()->function == 'Collaborator') {
             $consumers = Consumer::all();
             $id = Auth::id();
-            $day = date("d");
+            $day = date("Y/m/d");
+            $month = date("m");
+            $year = date("Y");
             $collaborator = User::find($id);
             $region = Region::find($collaborator->region_id);
             
@@ -72,7 +74,8 @@ class HomeController extends Controller
 
             $loans = Loan::where('status', 'opened')->where('region_id', $region->id)->get();
             $loansFinished = Loan::where('status', 'paid')->where('region_id', $region->id)->get();
-            $loansFinishedDay = Loan::whereDay("updated_at",$day)->where("status", "paid")->where('region_id', $region->id)->get();
+            // dd($day);
+            $loansFinishedDay = Loan::whereDate("updated_at", $day)->where("status", "paid")->where('region_id', $region->id)->get();
             // dd('aaaa', $loans);
             
             return view('loans', compact('consumers','collaborator', 'region', 'loans', 'loansFinished', 'loansFinishedDay'));
