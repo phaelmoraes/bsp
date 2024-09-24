23c579a4e6a5c5adf130fac267613c56c2b58d82
Ismael Filho <ismaelscf@gmail.com>
2024-09-24 00:09:39 -0300

    Ateração da exibição das coletas

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,7 +64,6 @@ class User extends Authenticatable
 
     public function loanInstallmentsToday($id){
         $day = date("Y-m-d");
-        $day = '2024-08-26';
         $value = LoanInstallment::whereDate('updated_at', $day)->where('user_id', $id)->sum('amount_paid');
         return $value;
     }
